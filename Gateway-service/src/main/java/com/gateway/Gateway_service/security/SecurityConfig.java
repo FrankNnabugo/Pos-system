6c24785b75ab770e0267286129bd357c593d8528
frank nnabugo <franksamuel224@gmail.com>
2026-03-11 00:14:20 +0100

    integrated keycloak and implemented authentication

---
+++ Gateway-service/src/main/java/com/gateway/Gateway_service/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.gateway.Gateway_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(
+                                "/eureka/**",
+                                "/actuator/**",
+                                "/health",
+                                "/api-docs",
+                                "/swagger",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        )
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth-> oauth.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+}
